877027fc3993fe7f0ac1591a7f872252a4334687
saasbook <saasbook@saasbook.(none)>
2012-11-05 14:12:45 +0800

    Hw3 part 1 complete:Happy paths for filtering movies

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -27,9 +27,38 @@ Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step to "submit" the search form on the homepage
   # enter step(s) to ensure that PG and R movies are visible
   # enter step(s) to ensure that other movies are not visible
+  Given I check the following ratings: "ratings_PG", "ratings_R"
+  And I uncheck the following ratings: "ratings_G", "ratings_PG-13"
+  When I press "ratings_submit"
+  Then I should be on the RottenPotatoes home page
+  And I should see the following movies:
+  | title                   | rating | release_date |
+  | The Terminator          | R      | 26-Oct-1984  |
+  | When Harry Met Sally    | R      | 21-Jul-1989  |
+  | Amelie                  | R      | 25-Apr-2001  |
+  | The Incredibles         | PG     | 5-Nov-2004   |
+  | Raiders of the Lost Ark | PG     | 12-Jun-1981  |
+  But I should not see the following movies:
+  | title                   | rating | release_date |
+  | Aladdin                 | G      | 25-Nov-1992  |
+  | The Help                | PG-13  | 10-Aug-2011  |
+  | Chocolat                | PG-13  | 5-Jan-2001   |
+  | 2001: A Space Odyssey   | G      | 6-Apr-1968   |
+  | Chicken Run             | G      | 21-Jun-2000  |
+
+
 
 Scenario: no ratings selected
   # see assignment
+  Given I uncheck the following ratings: "ratings_PG", "ratings_R", "ratings_G", "ratings_PG-13"
+  When I press "ratings_submit"
+  Then I should be on the RottenPotatoes home page
+  And I should not see any movies
+
 
 Scenario: all ratings selected
   # see assignment
+  Given I check the following ratings: "ratings_PG", "ratings_R", "ratings_G", "ratings_PG-13"
+  When I press "ratings_submit"
+  Then I should be on the RottenPotatoes home page
+  And I should see all the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,9 +4,37 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
+#flunk "Unimplemented"
 end
+
+Then /I should see the following movies/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    if !(movie[:rating] =~ /[PG||R]/) then flunk "sort by rating is not working" end
+  end
+#flunk "Unimplemented"
+end
+
+Then /I should not see the following movies/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    if !(movie[:rating] =~ /[PG\-13||G]/) then flunk "sort by rating is not working" end
+  end
+#flunk "Unimplemented"
+end
+
+Then /I should not see any movies/ do
+  rows= page.all("table#movies tbody tr td[1]").map {|t| t.text}.size
+  rows.should == 0
+end
+
+Then /I should see all the movies/ do
+  rows= page.all("table#movies tbody tr td[1]").map {|t| t.text}.size
+  rows.should == Movie.all.size
+end
+
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
@@ -14,7 +42,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  puts page.body.include? "/(.*)#{e1}(.*)#{e2}/"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +53,12 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/,\s*/).each do |rate|
+    if uncheck == "un"
+      step %Q(I uncheck #{rate})
+    else
+      step %Q(I check #{rate})
+    end
+  end
 end
+
